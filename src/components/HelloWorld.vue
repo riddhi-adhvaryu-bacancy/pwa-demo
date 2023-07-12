d57d6a12dec95e1fe57cd7bdccf28c126ba4cf0c
riddhi <riddhi.adhvaryu@bacancy.com>
2023-07-12 00:06:57 -0400

    PWA examples

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,20 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
+    <h2>
+      {{
+        !onLine
+          ? 'You are Offline...'
+          : showBackOnline
+          ? 'Back Online...'
+          : message
+      }}
+    </h2>
+    <h3>Links of Most Popular PWA examples</h3>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa" target="_blank" rel="noopener">pwa</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+      <li><a href="https://www.bmw.com/en/index.html" target="_blank" rel="noopener">BMW</a></li>
+      <li><a href="https://www.starbucks.com" target="_blank" rel="noopener">Starbucks</a></li>
+      <li><a href="https://www.pinterest.ca" target="_blank" rel="noopener">Pinterest</a></li>
     </ul>
   </div>
 </template>
@@ -36,6 +24,40 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      message: 'Welcome to Vue!',
+      onLine: navigator.onLine,
+      showBackOnline: false
+    }
+  },
+  methods: {
+    doSomething() {
+      alert('Hello!')
+    },
+    updateOnlineStatus(e) {
+      const { type } = e
+      this.onLine = type === 'online'
+    }
+  },
+  watch: {
+    onLine(v) {
+      if (v) {
+        this.showBackOnline = true
+        setTimeout(() => {
+          this.showBackOnline = false
+        }, 1000)
+      }
+    }
+  },
+  mounted() {
+    window.addEventListener('online', this.updateOnlineStatus)
+    window.addEventListener('offline', this.updateOnlineStatus)
+  },
+  beforeDestroy() {
+    window.removeEventListener('online', this.updateOnlineStatus)
+    window.removeEventListener('offline', this.updateOnlineStatus)
   }
 }
 </script>
@@ -46,14 +68,26 @@ h3 {
   margin: 40px 0 0;
 }
 ul {
-  list-style-type: none;
+  list-style-type: circle;
   padding: 0;
 }
 li {
-  display: inline-block;
+  display: block;
   margin: 0 10px;
 }
-a {
+a,button {
   color: #42b983;
+}
+
+button {
+  background: none;
+  border: solid 1px;
+  border-radius: 2em;
+  font: inherit;
+  padding: 0.75em 2em;
+  margin-bottom: 1rem;
+  margin-left: auto;
+  margin-right: auto;
+  display: block;
 }
 </style>
